c9758325c42df2c6eac46e7e4a20373152a69972
Tad-z <david.akintade1000@gmail.com>
2023-08-06 11:20:16 +0100

    test

--- Logic/productLogic.js
+++ Logic/productLogic.js
@@ -21,7 +21,7 @@ exports.postProduct = async (req, res) => {
 exports.getProducts = async (req, res) => {
   try {
     // Finds all products saved
-    // const products = await Products.find().exec();
+    const products = await Products.find().exec();
     // If there are no products it returns an empty array
     if (!products.length) return res.json([]);
     // Maps each product found

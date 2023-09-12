0174d86551821c321ce1e7222c468466c7d58ae2
Tad-z <david.akintade1000@gmail.com>
2023-09-12 11:09:03 +0100

    added trim to the user model

--- models/user.js
+++ models/user.js
@@ -4,10 +4,12 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        trim: true,
     },
     email: {
         type: String,
         required: true,
+        trim: true,
     },
     password: {
         type: String,
